c47b23878f794554cb2877db6b3ee6adea2e2025
deagLeShOx <deagle.shox.98@mail.ru>
2019-05-17 03:30:00 +0300

    Что нового!
    -Улучшение режим конвертации
    -Исправление ошибки

--- file.py
+++ file.py
@@ -8,30 +8,27 @@ from time import sleep
 from enum import Enum
 
 bot = telebot.TeleBot(secure.Token)
-#keyboard1 = telebot.types.ReplyKeyboardMarkup()
-#keyboard1.row('Uzbek''\U0001F1FA\U0001F1FF', 'Rus''\U0001f1f7\U0001f1fa')
 #Флаг Узбекистана '\U0001F1FA\U0001F1FF'
 #Флаг России  '\U0001f1f7\U0001f1fa'
+
 @bot.message_handler(commands=['start'])
 def start_message(m):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(*[types.KeyboardButton(name) for name in ['Uzbek''\U0001F1FA\U0001F1FF', 'Rus''\U0001f1f7\U0001f1fa']])
     msg = bot.send_message(m.chat.id, '\U0001F1FA\U0001F1FF''Tilni tanlang' '\n''\U0001f1f7\U0001f1fa' 'Выберите язык', reply_markup=keyboard)
     bot.register_next_step_handler(msg,send_text)
+
 @bot.message_handler(content_types=['text'])
 def send_text(m):
-    #markup = types.ReplyKeyboardRemove(selective=False)
+
     if m.text == 'Uzbek''\U0001F1FA\U0001F1FF':
         chooselang(m.chat.id, "uz")
     elif m.text == 'Rus''\U0001f1f7\U0001f1fa':
         chooselang(m.chat.id, "ru")  
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(*[types.KeyboardButton(name) for name in [gettr(m.chat.id, 'calc'), gettr(m.chat.id, 'converter')]])
+    keyboard.add(*[types.KeyboardButton(name) for name in [gettr(m.chat.id, 'calc'), gettr(m.chat.id, 'converter'),]])
     msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'langselect'), parse_mode= 'Markdown', reply_markup=keyboard)
     bot.register_next_step_handler(msg,name)
-
-    #Konverter unicod ''\U0001F504'
-    #Kalkulyator unicod \U0001F522
     #Конвертер юникоде \U0001F504
     #Калькулятор юникоде ''\U0001F522''
     
@@ -50,13 +47,6 @@ def name(m):
         keyboard.add(*[types.KeyboardButton(name) for name in ['2 ➝ 10', '10 ➝ 2', '2 ➝ 16', '16 ➝ 2', '10 ➝ 16', '16 ➝ 10','2 ➝ 8', '8 ➝ 2', '10 ➝ 8', '8 ➝ 10', '8 ➝ 16', '16 ➝ 8', gettr(m.chat.id, 'back')]])
         msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'converter_type'), reply_markup=keyboard)
         bot.register_next_step_handler(msg, convert)
-# **Системы счисления **
-#2 int(msg, 2)
-#10 format (int (msg), "b")
-#16 format (int (msg), '02x')
-#8 format (int (m.text, 8), "b")
-
-#RU
 
 # 2 ➝ 10
 def dvds(m):
@@ -70,7 +60,7 @@ def dvds(m):
         c = gettr(m.chat.id, 'error')
     msg = bot.send_message(m.chat.id, c)
     markup = types.ForceReply(selective=False)
-    msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'bin'), reply_markup=markup)
+    msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'bin'), parse_mode = 'Markdown', reply_markup=markup)
     bot.register_next_step_handler(msg, dvds)
 
 #'10 ➝ 2'
@@ -80,14 +70,13 @@ def dsdv(m):
         send_text(m)
         return
     try:
-        c = str(int(m.text, 2))
+        c = format(int (m.text), "b")
     except:
         c = gettr(m.chat.id, 'error')
     msg = bot.send_message(m.chat.id, c)
     markup = types.ForceReply(selective=False)
-    msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'dec'), reply_markup=markup)
+    msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'dec'), parse_mode = 'Markdown', reply_markup=markup)
     bot.register_next_step_handler(msg, dsdv)
-
 
 #'2 ➝ 16'
 def dvsh(m):
@@ -96,12 +85,12 @@ def dvsh(m):
         send_text(m)
         return
     try:
-        c = format (int (m.text, 2), '0x') 
+        c = format(int (m.text, 2), "0x") 
     except:
         c = gettr(m.chat.id, 'error')
     msg = bot.send_message(m.chat.id, c)
     markup = types.ForceReply(selective=False)
-    msg = bot.send_message(m.chat.id, 'bin', reply_markup=markup)
+    msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'bin'), parse_mode = 'Markdown', reply_markup=markup)
     bot.register_next_step_handler(msg, dvsh)
 
 #'16 ➝ 2'   
@@ -110,12 +99,12 @@ def shdv(m):
         send_text(m)
         return
     try:
-        c = format (int (m.text, 16), "b") 
+        c = format(int (m.text, 16), "b") 
     except:
         c = gettr(m.chat.id, 'error')
     msg = bot.send_message(m.chat.id, c)
     markup = types.ForceReply(selective=False)
-    msg = bot.send_message(m.chat.id, 'hex', reply_markup=markup)
+    msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'hex'), parse_mode = 'Markdown', reply_markup=markup)
     bot.register_next_step_handler(msg, shdv)
 
 #'10 ➝ 16', 
@@ -124,14 +113,13 @@ def dssh(m):
         send_text(m)
         return
     try:
-        c = format (int (m.text), '0x')  
+        c = format(int (m.text), "0x")
     except:
         c = gettr(m.chat.id, 'error')
     msg = bot.send_message(m.chat.id, c)
     markup = types.ForceReply(selective=False)
-    msg = bot.send_message(m.chat.id, 'dec', reply_markup=markup)
+    msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'dec'), parse_mode = 'Markdown', reply_markup=markup)
     bot.register_next_step_handler(msg, dssh)
-
 
 #'16 ➝ 10'
 def shds(m):
@@ -144,7 +132,7 @@ def shds(m):
         c = gettr(m.chat.id, 'error')
     msg = bot.send_message(m.chat.id, c)
     markup = types.ForceReply(selective=False)
-    msg = bot.send_message(m.chat.id, 'hex', reply_markup=markup)
+    msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'hex'), parse_mode = 'Markdown', reply_markup=markup)
     bot.register_next_step_handler(msg, shds)
 
 #'2 ➝ 8'
@@ -153,11 +141,12 @@ def dvvs(m):
         send_text(m)
         return
     try:
-        c = str(int(m.text, 8))
+        c = format(int (m.text, 2), "0")
     except:
         c = gettr(m.chat.id, 'error')
     msg = bot.send_message(m.chat.id, c)
-    markup = bot.send_message(m.chat.id, 'bin', reply_markup=markup)
+    markup = types.ForceReply(selective=False)
+    msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'bin'), parse_mode = 'Markdown', reply_markup=markup)
     bot.register_next_step_handler(msg, dvvs)
 
 #'8 ➝ 2'
@@ -166,11 +155,12 @@ def vsdv(m):
         send_text(m)
         return
     try:
-        c = format (int (m.text, 8), "b")
+        c = format(int (m.text, 8), "b")
     except:
         c = gettr(m.chat.id, 'error')
     msg = bot.send_message(m.chat.id, c)
-    markup = bot.send_message(m.chat.id, 'oct', reply_markup=markup)
+    markup = types.ForceReply(selective=False)
+    msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'oct'), parse_mode = 'Markdown', reply_markup=markup)
     bot.register_next_step_handler(msg, vsdv)
 
 #'10 ➝ 8'
@@ -179,11 +169,12 @@ def dsvs(m):
         send_text(m)
         return
     try:
-        c = str (int (m.text, 8))
+        c = format(int (m.text), "0o")
     except:
         c = gettr(m.chat.id, 'error')
     msg = bot.send_message(m.chat.id, c)
-    markup = bot.send_message(m.chat.id, 'dec', reply_markup=markup)
+    markup = types.ForceReply(selective=False)
+    msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'dec'), parse_mode = 'Markdown', reply_markup=markup)
     bot.register_next_step_handler(msg, dsvs)
 
 #'8 ➝ 10'
@@ -192,11 +183,12 @@ def vsds(m):
         send_text(m)
         return
     try:
-        c = str (int (m.text, 8))
+        c = str(int(m.text, 8))
     except:
         c = gettr(m.chat.id, 'error')
     msg = bot.send_message(m.chat.id, c)
-    markup = bot.send_message(m.chat.id, 'oct', reply_markup=markup)
+    markup = types.ForceReply(selective=False)
+    msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'oct'), parse_mode = 'Markdown', reply_markup=markup)
     bot.register_next_step_handler(msg, vsds)
 
 #'8 ➝ 16'
@@ -205,11 +197,12 @@ def vssh(m):
         send_text(m)
         return
     try:
-        c = format (int (m.text, 8), "0x")
+        c = format(int (m.text, 8), "0x")
     except:
         c = gettr(m.chat.id, 'error')
     msg = bot.send_message(m.chat.id, c)
-    markup = bot.send_message(m.chat.id, 'oct', reply_markup=markup)
+    markup = types.ForceReply(selective=False)
+    msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'oct'), parse_mode = 'Markdown', reply_markup=markup)
     bot.register_next_step_handler(msg, vssh)
 
 #'16 ➝ 8'
@@ -218,12 +211,15 @@ def shvs(m):
         send_text(m)
         return
     try:
-        c = format (int (m.text, 16), "010")
+        c = format(int (m.text, 16), "0o") 
     except:
         c = gettr(m.chat.id, 'error')
     msg = bot.send_message(m.chat.id, c)
-    markup = bot.send_message(m.chat.id, 'hex', reply_markup=markup)
+    markup = types.ForceReply(selective=False)
+    msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'hex'), parse_mode = 'Markdown', reply_markup=markup)
     bot.register_next_step_handler(msg, shvs)
+
+
 
 def back(m):
     if m.text == gettr(m.chat.id, 'back'):
@@ -251,89 +247,94 @@ def calculator(m):
     bot.register_next_step_handler(msg, calculator)
 
 def convert(m):
+    markup = types.ReplyKeyboardRemove(selective=False)
     if m.text == '2 ➝ 10':
-        markup = types.ForceReply(selective=False)
-        msg = bot.send_message(m.chat.id, gettr(m.chat.id,'bin'), reply_markup=markup)
+        bot.send_message(m.chat.id, gettr(m.chat.id, 'bin'), parse_mode='Markdown', reply_markup=markup)
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(name) for name in [gettr(m.chat.id, 'back')]])
+        msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'example_bin'), parse_mode='Markdown', reply_markup=keyboard)
         bot.register_next_step_handler(msg, dvds)
     elif m.text == '10 ➝ 2':
-        markup = types.ForceReply(selective=False)
-        msg = bot.send_message(m.chat.id, gettr(m.chat.id,'dec'), reply_markup=markup)
+        bot.send_message(m.chat.id, gettr(m.chat.id, 'dec'), parse_mode='Markdown', reply_markup=markup)
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(name) for name in [gettr(m.chat.id, 'back')]])
+        msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'example_dec'), parse_mode='Markdown', reply_markup=keyboard)
         bot.register_next_step_handler(msg, dsdv) 
     elif m.text == '2 ➝ 16':
-        markup = types.ForceReply(selective=False)
-        msg = bot.send_message(m.chat.id, gettr(m.chat.id,'bin'), reply_markup=markup)
+        bot.send_message(m.chat.id, gettr(m.chat.id, 'bin'), parse_mode='Markdown', reply_markup=markup)
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(name) for name in [gettr(m.chat.id, 'back')]])
+        msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'example_bin'), parse_mode='Markdown', reply_markup=keyboard)
         bot.register_next_step_handler(msg, dvsh)
     elif m.text == '16 ➝ 2':
-        markup = types.ForceReply(selective=False)
-        msg = bot.send_message(m.chat.id, gettr(m.chat.id,'hex'), reply_markup=markup)
+        bot.send_message(m.chat.id, gettr(m.chat.id, 'hex'), parse_mode='Markdown', reply_markup=markup)
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(name) for name in [gettr(m.chat.id, 'back')]])
+        msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'example_hex'), parse_mode='Markdown', reply_markup=keyboard)
         bot.register_next_step_handler(msg, shdv)
     elif m.text == '10 ➝ 16':
-        markup = types.ForceReply(selective=False)
-        msg = bot.send_message(m.chat.id, gettr(m.chat.id,'dec'), reply_markup=markup)
+        bot.send_message(m.chat.id, gettr(m.chat.id, 'dec'), parse_mode='Markdown', reply_markup=markup)
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(name) for name in [gettr(m.chat.id, 'back')]])
+        msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'example_dec'), parse_mode='Markdown', reply_markup=keyboard)
         bot.register_next_step_handler(msg, dssh)
     elif m.text == '16 ➝ 10':
-        markup = types.ForceReply(selective=False)
-        msg = bot.send_message(m.chat.id, gettr(m.chat.id,'hex'), reply_markup=markup)
+        bot.send_message(m.chat.id, gettr(m.chat.id, 'hex'), parse_mode='Markdown', reply_markup=markup)
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(name) for name in [gettr(m.chat.id, 'back')]])
+        msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'example_hex'), parse_mode='Markdown', reply_markup=keyboard)
         bot.register_next_step_handler(msg, shds)
     elif m.text == '2 ➝ 8':
-        markup = types.ForceReply(selective=False)
-        msg = bot.send_message(m.chat.id, gettr(m.chat.id,'bin'), reply_markup=markup)
+        bot.send_message(m.chat.id, gettr(m.chat.id, 'bin'), parse_mode='Markdown', reply_markup=markup)
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(name) for name in [gettr(m.chat.id, 'back')]])
+        msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'example_bin'), parse_mode='Markdown', reply_markup=keyboard)
         bot.register_next_step_handler(msg, dvvs)
     elif m.text == '8 ➝ 2':
-        markup = types.ForceReply(selective=False)
-        msg = bot.send_message(m.chat.id, gettr(m.chat.id,'oct'), reply_markup=markup)
+        bot.send_message(m.chat.id, gettr(m.chat.id, 'oct'), parse_mode='Markdown', reply_markup=markup)
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(name) for name in [gettr(m.chat.id, 'back')]])
+        msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'example_oct'), parse_mode='Markdown', reply_markup=keyboard)
         bot.register_next_step_handler(msg, vsdv)
     elif m.text == '8 ➝ 10':
-        markup = types.ForceReply(selective=False)
-        msg = bot.send_message(m.chat.id, gettr(m.chat.id,'oct'), reply_markup=markup)
+        bot.send_message(m.chat.id, gettr(m.chat.id, 'oct'), parse_mode='Markdown', reply_markup=markup)
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(name) for name in [gettr(m.chat.id, 'back')]])
+        msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'example_oct'), parse_mode='Markdown', reply_markup=keyboard)
         bot.register_next_step_handler(msg, vsds)
     elif m.text == '10 ➝ 8':
-        markup = types.ForceReply(selective=False)
-        msg = bot.send_message(m.chat.id, gettr(m.chat.id,'dec'), reply_markup=markup)
+        bot.send_message(m.chat.id, gettr(m.chat.id, 'dec'), parse_mode='Markdown', reply_markup=markup)
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(name) for name in [gettr(m.chat.id, 'back')]])
+        msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'example_dec'), parse_mode='Markdown', reply_markup=keyboard)
         bot.register_next_step_handler(msg, dsvs)
     elif m.text == '8 ➝ 16':
-        markup = types.ForceReply(selective=False)
-        msg = bot.send_message(m.chat.id, gettr(m.chat.id,'oct'), reply_markup=markup)
+        bot.send_message(m.chat.id, gettr(m.chat.id, 'oct'), parse_mode='Markdown', reply_markup=markup)
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(name) for name in [gettr(m.chat.id, 'back')]])
+        msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'example_oct'), parse_mode='Markdown', reply_markup=keyboard)
         bot.register_next_step_handler(msg, vssh)
     elif m.text == '16 ➝ 8':
-        markup = types.ForceReply(selective=False)
-        msg = bot.send_message(m.chat.id, gettr(m.chat.id,'hex'), reply_markup=markup)
-        bot.register_next_step_handler(msg, shvs)
-    elif m.text == 'back':
+        bot.send_message(m.chat.id, gettr(m.chat.id, 'hex'), parse_mode='Markdown', reply_markup=markup)
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        keyboard.add(*[types.KeyboardButton(name) for name in ['Калькулятор''\U0001F522', 'Конвертер''\U0001F504']])
-        msg = bot.send_message(m.chat.id, 'Выберите режим' '\U0001F522''\U0001F504', reply_markup=keyboard)
-        bot.register_next_step_handler(msg,name)(msg, back)
-
-
-def calculatoruz(m):
-    if m.text == 'Orqaga':
-        startuz(m)
+        keyboard.add(*[types.KeyboardButton(name) for name in [gettr(m.chat.id, 'back')]])
+        msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'example_hex'), parse_mode='Markdown', reply_markup=keyboard)
+        bot.register_next_step_handler(msg, shvs)
+    elif m.text == gettr(m.chat.id, 'back'):
+        send_text(m)
         return
-    s = ""
-    try:
-        pattern = re.compile("^([-+]*[0-9]*\.?[0-9]+[\/\+\-\*\(\)\^\%]*)*$")
-        if pattern.match(m.text):
-            s = eval(m.text)
-        else:
-            s = "Ifodada taqiqlangan belgilar mavjud"
-    except:
-        s = "Ifodada xatolik"
-    msg = bot.send_message(m.chat.id, s)
-    markup = types.ForceReply(selective=False)
-    msg = bot.send_message(m.chat.id, 'Arifmetik ifoda kiriting:', reply_markup=keyboard)
-    bot.register_next_step_handler(msg, calculatoruz)
+    #c = ""
+    #try:
+        #pattern = re.compile(" 'bin', 'oct', 'dec', 'hex'")
+        #if pattern.match(m.text):
+         #   c = eval(m.text)
+       # else:
+         #   c = gettr(m.chat.id, 'converter_type')
+   # except:
+       # c = gettr(m.chat.id, 'converter_mod')
+   # msg = bot.send_message(m.chat.id, c)
+   # markup = types.ForceReply(selective=False)
+    #msg = bot.send_message(m.chat.id, gettr(m.chat.id, 'converter_type'), reply_markup=keyboard)
+    #bot.register_next_step_handler(msg, convert)#(msg, name)
 
-
-#'Ikkilik sanoq sistemasida raqam kiriting:'
-#'Sakkizlik sanoq sistemasida raqam kiriting:'
-#"O'nlik sanoq sistemasida raqam kiriting:"
-#"O'n oltilik sanoq sistemasida belgi kiriting:"
-#def convertuz(m):
-
-  #  elif m.text == 'Orqaga':
-     #   keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-      #  keyboard.add(*[types.KeyboardButton(name) for name in ['Kalkulyator''\U0001F522', 'Konverter''\U0001F504']])
-      # msg = bot.send_message(m.chat.id, 'Rejimni tanlang''\U0001F522''\U0001F504', reply_markup=keyboard)
-       # bot.register_next_step_handler(msg,nameuz)(msg, backuz)
 bot.polling()
--- lang.py
+++ lang.py
@@ -1,11 +1,12 @@
 lang_ru = {}
 lang_uz = {}
 
+
 lang_ru['calc'] = 'Калькулятор''\U0001F522'
 lang_uz['calc'] = 'Kalkulyator''\U0001F522'
 
-#lang_ru['select_type'] = "Выберите режим"
-#lang_uz['select_type'] = "Rejimni tanlang"
+lang_ru['select_type'] = "Выберите режим"
+lang_uz['select_type'] = "Rejimni tanlang"
 
 lang_ru['calc_mod'] = '`Вы в режиме Калькулятор`'
 lang_uz['calc_mod'] = '`Siz Kalkulyator rejimidasiz`'
@@ -25,23 +26,35 @@ lang_uz['converter_type'] = 'Konvertatsiya qilish turini tanlang'
 lang_ru['langselect'] = "Вы выбрали *Русский язык* \nТеперь выберите режим \U0001F522 \U0001F504"
 lang_uz['langselect'] = "Siz *O'zbek tili*ni tanladingiz \nEndi kerakli rejimni tanlang \U0001F522 \U0001F504"
 
+lang_ru['example_bin'] = 'Пример в двоичной системе: `11111100011`'
+lang_uz['example_bin'] = 'Ikkilik sanoq sistemasida Namuna: `11111100011`'
+
+lang_ru['example_oct'] = 'Пример в восьмеричной системе: `01234567`'
+lang_uz['example_oct'] = 'Sakkizlik sanoq sistemasida Namuna: `01234567`'
+
+lang_ru['example_dec'] = 'Пример в десятичной системе: `0123456789`'
+lang_uz['example_dec'] = "O'nlik sanoq sistemasida Namuna: `0123456789`"
+
+lang_ru['example_hex'] = 'Пример в шестнадцатеричной системе: `0123456789ABCDEF`'
+lang_uz['example_hex'] = "O'n oltilik sanoq sistemasida Namuna: `0123456789ABCDEF`"
+
 lang_ru['back'] = 'Назад'
 lang_uz['back'] = 'Orqaga'
 
-lang_ru['error'] = "Ошибка"
-lang_ru['error'] = "Xatolik"
+lang_ru['error'] = "Ошибка!\n_для выхода из режима Конвертации пишите_   *Назад*"
+lang_uz['error'] = "Xatolik\n_Konvertatsiyalash rejimidan chiqish uchun_  *Orqaga*  _yozing_"
 
-lang_ru['bin'] = 'Введите число в двоичной системе:'
-lang_uz['bin'] = 'Ikkilik sanoq sistemasida raqam kiriting'
+lang_ru['bin'] = 'Введите число в двоичной системе:''\n_для выхода из режима Конвертации пишите_   *Назад*'
+lang_uz['bin'] = 'Ikkilik sanoq sistemasida raqam kiriting''\n_Konvertatsiyalash rejimidan chiqish uchun_  *Orqaga*  _yozing_'
 
-lang_ru['dec'] = 'Введите число в десятичной системе:'
-lang_uz['dec'] = "O'nlik sanoq sistemasida raqam kiriting"
+lang_ru['dec'] = 'Введите число в десятичной системе:''\n_для выхода из режима Конвертации пишите_   *Назад*'
+lang_uz['dec'] = "O'nlik sanoq sistemasida raqam kiriting"'\n_Konvertatsiyalash rejimidan chiqish uchun_  *Orqaga*  _yozing_'
 
-lang_ru['hex'] = 'Введите число в шестнадцатеричной системе'
-lang_uz['hex'] = "O'n oltilik sanoq sistemasida belgi kiriting"
+lang_ru['hex'] = 'Введите число в шестнадцатеричной системе''\n_для выхода из режима Конвертации пишите_   *Назад*'
+lang_uz['hex'] = "O'n oltilik sanoq sistemasida belgi kiriting"'\n_Konvertatsiyalash rejimidan chiqish uchun_  *Orqaga*  _yozing_'
 
-lang_ru['oct'] = 'Введите число в восьмеричной системе'
-lang_uz['oct'] = 'Sakkizlik sanoq sistemasida raqam kiriting'
+lang_ru['oct'] = 'Введите число в восьмеричной системе''\n_для выхода из режима Конвертации пишите_   *Назад*'
+lang_uz['oct'] = 'Sakkizlik sanoq sistemasida raqam kiriting''\n_Konvertatsiyalash rejimidan chiqish uchun_  *Orqaga*  _yozing_'
 
 lang_ru['taboo_char'] = "В выражении присутствуют запрещённые символы"
 lang_uz['taboo_char'] = "Ifodada taqiqlangan belgilar mavjud"
